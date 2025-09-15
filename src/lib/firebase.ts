2a2fffedd60d44ac0d40192c3fcee65edf8a6ae0
RISHI SHANKAR SAI KAPIL VIBHUTHI <vibhuthirssk123@gmail.com>
2025-09-15 11:08:49 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,19 @@
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: 'studio-2525731056-d1386',
+  appId: '1:84236984952:web:114dd3feb377fca338038d',
+  storageBucket: 'studio-2525731056-d1386.firebasestorage.app',
+  apiKey: 'AIzaSyANKQfL37GKDMkyhGU4COg_oQpRQibiiao',
+  authDomain: 'studio-2525731056-d1386.firebaseapp.com',
+  messagingSenderId: '84236984952',
 };
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
