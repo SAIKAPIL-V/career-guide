af450887010a990877d9849190f67a22be8a19b0
RISHI SHANKAR SAI KAPIL VIBHUTHI <vibhuthirssk123@gmail.com>
2025-09-15 10:10:21 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-2525731056-d1386",
+  "appId": "1:84236984952:web:114dd3feb377fca338038d",
+  "storageBucket": "studio-2525731056-d1386.firebasestorage.app",
+  "apiKey": "AIzaSyANKQfL37GKDMkyhGU4COg_oQpRQibiiao",
+  "authDomain": "studio-2525731056-d1386.firebaseapp.com",
+  "messagingSenderId": "84236984952"
 };
 
 
