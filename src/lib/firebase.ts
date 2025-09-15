e1364c0be8a19563c030e36293c5bccc32b23375
RISHI SHANKAR SAI KAPIL VIBHUTHI <vibhuthirssk123@gmail.com>
2025-09-15 10:34:25 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyANKQfL37GKDMkyhGU4COg_oQpRQibiiao',
-  authDomain: 'studio-2525731056-d1386.firebaseapp.com',
-  projectId: 'studio-2525731056-d1386',
-  storageBucket: 'studio-2525731056-d1386.appspot.com',
-  messagingSenderId: '84236984952',
-  appId: '1:84236984952:web:114dd3feb377fca338038d',
+  apiKey: "AIzaSyANKQfL37GKDMkyhGU4COg_oQpRQibiiao",
+  authDomain: "studio-2525731056-d1386.firebaseapp.com",
+  projectId: "studio-2525731056-d1386",
+  storageBucket: "studio-2525731056-d1386.appspot.com",
+  messagingSenderId: "84236984952",
+  appId: "1:84236984952:web:114dd3feb377fca338038d"
 };
 
 
